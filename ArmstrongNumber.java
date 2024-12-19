7df66aebd2f03f4e431918dcefb6ecb638ad2f23
Sonalinayak12 <sn949711@gmail.com>
2024-12-19 16:10:36 +0530

    Add files via upload

--- ArmstrongNumber.java
+++ ArmstrongNumber.java
@@ -1,30 +1,27 @@
-public class ArmstrongNumber
+ public class ArmstrongNumber
  {
- public static void main(String args[])
- {
- int n, arg, sum = 0, r;
- 
- n = 153; // input value
- arg = n;
- for (int i = 1; i < n; i++)
- {
- while (n > 0)
- {
- r = n % 10;
- sum = sum + (r * r * r);
- n = n / 10;
- 
- }
- 
- }
- if (arg == sum)
- {
- System.out.println("Given number is armstrong number: " + arg);
- }
- else
- {
- System.out.println("Given number is not armstrong number: " + arg);
- }
- }
- 
+     public static void main(String args[])
+     {
+         int n,arg,sum=0,r;
+         n=153;
+         arg=n;
+         for(int i=1;i<n;i++)
+         {
+             while(n>0)
+             {
+                 r=n%10;
+                 sum=sum+(r*r*r);
+                 n=n/10;
+             }
+         }
+         if(arg==sum)
+         {
+             System.out.println("Given number is armstrong number:"+arg);
+         }
+         else
+         {
+             System.out.println("Given number is not a armstong number:"+arg);
+         }
+               
+     }
  }
